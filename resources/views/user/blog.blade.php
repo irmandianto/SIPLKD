e411948d496d2dc81cef7f635b5f276c5d0a51c3
irmandianto <irmandianto0104@gmail.com>
2020-06-19 23:15:37 +0700

    menambahkan file app dan blog

---
+++ resources/views/user/blog.blade.php
@@ -0,0 +1,45 @@
+@extends('user/app')
+
+@section('bg-img',asset('user/img/home-bg.jpg'))
+
+@section('main-content')
+<!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+                @foreach ($posts as $post)
+          <div class="post-preview">
+
+            <a href="{{ route('post',$post->slug)}}">
+              <h2 class="post-title">
+              {{ $post->title}}
+              </h2>
+              <h3 class="post-subtitle">
+                   {{ $post->subtitle}}
+              </h3>
+            </a>
+
+          </div>
+
+          <div class="post-preview">
+
+            <p class="post-meta">Posted by
+              <a href="#">Qatulistiwa_Islam</a>
+             {{$post->created_at->diffForHumans()}}</p>
+          </div>
+          @endforeach
+
+
+
+
+          <!-- Pager -->
+          <ul class="pager">
+
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+
+    @endsection
